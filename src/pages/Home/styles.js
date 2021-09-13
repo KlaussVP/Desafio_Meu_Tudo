048b6ae5a427966c6e14063dce63e2ca853238ab
KlaussVP <klauss_mbc@hotmail.com>
2021-09-13 17:48:56 -0300

    feat: initial setup

---
+++ src/pages/Home/styles.js
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+const Container = styled.section`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 300px;
+  width: 300px;
+  background: #FF0000;
+  font-size: 4rem;
+  font-weight: 700;
+`;
+
+export default Container;
