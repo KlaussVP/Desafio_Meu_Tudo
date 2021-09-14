1c88f67cb85116a29004e6c49197a241a15d5b83
KlaussVP <klauss_mbc@hotmail.com>
2021-09-14 17:16:41 -0300

    fix: working webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -3,28 +3,29 @@ const path = require('path');
 module.exports = {
   entry: './src/index.js', 
   output: {
+    publicPath: '/',
     path: path.resolve(__dirname, 'public'), 
     filename: 'bundle.js'
   },
   module: {
-    rules: [{
-      test: /\.m?js$/,                         
-      exclude: /(node_modules)/,             
-      use: {
-        loader: 'babel-loader',                
-        options: {
-          presets: ['@babel/preset-env', '@babel/preset-react']      
+    rules: [
+      {
+        test: /\.m?js$/,                         
+        exclude: /(node_modules)/,             
+        use: {
+          loader: 'babel-loader',                
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react']      
+          }
         }
       }
-    }]
+    ]
   },
   devServer: {
-    contentBase: path.join(__dirname, 'public'),
-    publicPath: "/",
+    static: './public',
     compress: true,
-    port: 9000,
+    port: 8080,
     historyApiFallback: true,
   },
-  devtool: "source-map",
+  devtool: "source-map"
 };
-
